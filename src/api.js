5b9fc8cf6d97b282cd3757c2bd6c374490669f6b
Jakub Zeiner <jakubzeiner@gmail.comgit config --global user.email jakubzeiner@gmail.com>
2022-09-02 16:29:05 +0200

    current weather connection

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6db741131dmshbb453f40e5c8388p10942ajsn89c3a95050e6',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6db741131dmshbb453f40e5c8388p10942ajsn89c3a95050e6",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
 };
 
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "30fc2eea79df5359ce9f0133a688712a";
