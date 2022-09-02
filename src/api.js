16be0f09c80282a5adc97efb5389d73db74f0448
Jakub Zeiner <jakubzeiner@gmail.comgit config --global user.email jakubzeiner@gmail.com>
2022-09-02 10:11:32 +0200

    connecting to api, search works

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6db741131dmshbb453f40e5c8388p10942ajsn89c3a95050e6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
